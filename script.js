518f204b200fd7e86819eecb116c9899b0dd65fb
Derrick Danso <derrickdansodd3@gmail.com>
2023-07-20 06:21:54 +0000

    Completed basic functionality

---
+++ script.js
@@ -0,0 +1,38 @@
+//Take input from user
+
+//declare variable input
+let input;
+
+//declare variable convResult
+let convResult;
+
+//declare function to alert prompt
+function alertPrompt (e) {
+    if(e.srcElement.innerHTML === 'Keycode to Character') {
+        input = prompt('Enter your Keycode');
+        convResult =  String.fromCharCode(input); 
+        console.log(convResult);         
+    } else {
+        input = prompt('Enter your Character');
+        convResult = input.charCodeAt();
+    };
+    dispResults();
+};
+
+//click event listener
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', alertPrompt));
+
+//display results
+function dispResults() {
+    const Result = document.querySelector('.result');
+
+    if(convResult === '\x00') {
+        Result.textContent = 'INPUT AGAIN!!!. Your input is invalid!';
+    } else {
+        Result.textContent = convResult;
+    };
+    
+};
+
+
